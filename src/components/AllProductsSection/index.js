e838a3735b2227f6961e134eed73bef2c53047ef
jamisaiteja <jamisaiteja666@gmail.com>
2023-01-05 06:46:46 +0000

    session

---
+++ src/components/AllProductsSection/index.js
@@ -0,0 +1,70 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import Loader from 'react-loader-spinner'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+import ProductCard from '../ProductCard'
+import './index.css'
+
+class AllProductsSection extends Component {
+  state = {
+    productsList: [],
+    isLoading: true,
+  }
+
+  componentDidMount() {
+    this.getProducts()
+  }
+
+  getProducts = async () => {
+    const apiUrl = 'https://apis.ccbp.in/products'
+    const jwtToken = Cookies.get('jwt_token')
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    }
+    const response = await fetch(apiUrl, options)
+    if (response.ok === true) {
+      const fetchedData = await response.json()
+      const updatedData = fetchedData.products.map(product => ({
+        title: product.title,
+        brand: product.brand,
+        price: product.price,
+        id: product.id,
+        imageUrl: product.image_url,
+        rating: product.rating,
+      }))
+      this.setState({
+        productsList: updatedData,
+        isLoading: false,
+      })
+    }
+  }
+
+  renderProductsList = () => {
+    const {productsList, isLoading} = this.state
+    return (
+      <div>
+        <h1 className="products-list-heading">All Products</h1>
+
+        {isLoading ? (
+          <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+        ) : (
+          <ul className="products-list">
+            {productsList.map(product => (
+              <ProductCard productData={product} key={product.id} />
+            ))}
+          </ul>
+        )}
+      </div>
+    )
+  }
+
+  render() {
+    return <>{this.renderProductsList()}</>
+  }
+}
+
+export default AllProductsSection
---
+++ src/components/ProtectedRoute/index.js
@@ -0,0 +1,14 @@
+import {Route, Redirect} from 'react-router-dom'
+import Cookies from 'js-cookie'
+// import Home from '../Home'
+
+const ProtectedRoute = props => {
+  const accessToken = Cookies.get('jwt_token')
+
+  if (accessToken === undefined) {
+    return <Redirect to="/login" />
+  }
+  return <Route {...props} />
+}
+
+export default ProtectedRoute
